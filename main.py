6f8b7874532c8403041e28c8725935bbead197e2
+D <112191393+addy-dr@users.noreply.github.com>
2024-11-16 15:51:36 +0000

    Model for mars

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ import matplotlib.pyplot as plt
 
 SEMIMAJOR_AXIS_EARTH = 227939366000 #m
 ECCENTRICITY_EARTH = 0.0934
-RADIUS_EARTH = 1737000 #m
+RADIUS_EARTH = 3389.5000 #m
 SOLAR_LUMINOSITY = 3.828 * 10**26 #W/m2s
 
-EARTH_DENSITY = 3344 #kg/m3
+EARTH_DENSITY = 3934 #kg/m3
 HEAT_CAPACITY_ROCK = 2000 #
 STEFANS_CONSTANT = 5.670374419 * 10**-8 #
 
-CRUST_DEPTH = 0.002 #m
-ROTATIONAL_VELOCITY_EARTH = (2*np.pi) / (24*3600*30)
+CRUST_DEPTH = 0.2 #m
+ROTATIONAL_VELOCITY_EARTH = (2*np.pi) / (24.5*3600)
+EMISSIVITY = 0.75
 
 def emission_from_point(I, emissivity, T, dt):
     dT_dt = (I - emissivity*STEFANS_CONSTANT*T**4) / (EARTH_DENSITY * HEAT_CAPACITY_ROCK * CRUST_DEPTH)
@@ -33,14 +34,14 @@ def main():
     for val in phi_space:
         intensity_chart.append(solar_intensity(np.pi/2, val))
 
-    temperature_history = [273+120]
+    temperature_history = [210]
     intensity_history = []
-    time_history = np.linspace(0,24*365,10000)
+    time_history = np.linspace(0,240,240)
     for minute in time_history:
         phi = ROTATIONAL_VELOCITY_EARTH * minute*3600
         intensity = solar_intensity(np.pi/2, phi)
         intensity_history.append(intensity)
-        temperature = temperature_history[-1] + emission_from_point(intensity, 0.05, temperature_history[-1], 3600)
+        temperature = temperature_history[-1] + emission_from_point(intensity, EMISSIVITY, temperature_history[-1], 3600)
         temperature_history.append(temperature)
     temperature_history = temperature_history[:-1]
     
